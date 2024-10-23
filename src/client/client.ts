d01aa80e3b7c0866d0b7d6cae64534e87edc7237
radek.Bednarik <radek.bednarik@tesena.com>
2024-10-23 09:19:16 +0200

    chore: more config settings

---
+++ src/client/client.ts
@@ -0,0 +1,15 @@
+export default class Client {
+  private readonly token: string;
+
+  constructor({ token }: { token?: string }) {
+    this.token = this.addToken(token);
+  }
+
+  private addToken(token?: string) {
+    if (typeof token === "string") {
+      return token;
+    }
+
+    throw new Error("Provided token must be of the type string.");
+  }
+}
